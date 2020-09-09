0dc7b88d4f61c36ec0b42d582e24a07c4ba9399b
Victor Savoi <savoi.victor@hotmail.com>
2020-09-08 21:46:16 -0300

    Dynamic menu included

--- src/app/navegacao/menu/menu.component.ts
+++ src/app/navegacao/menu/menu.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { ItemMenu } from './models/menu';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent { }
+export class MenuComponent {
+
+  itensMenu: ItemMenu[] = [{
+    name: 'Home',
+    link: '/home',
+    admin: false,
+    exact: true
+  }, {
+    name: 'Cadastro',
+    link: '/cadastro',
+    admin: true,
+    exact: true
+  }, {
+    name: 'Sobre',
+    link: '/sobre',
+    admin: false,
+    exact: true
+  }, {
+    name: 'Produto',
+    link: '/produto',
+    admin: false,
+    exact: true
+  }];
+
+}
---
+++ src/app/navegacao/menu/models/menu.ts
@@ -0,0 +1,6 @@
+export interface ItemMenu {
+    link: string,
+    name: string,
+    exact: boolean,
+    admin: boolean
+}
